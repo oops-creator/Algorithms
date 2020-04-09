f445666045967c137f62ec348d90745809a73598
oops-creator <still.shivam@gmail.com>
2020-04-09 14:38:02 +0530

    Add GUI to plot graphs

---
+++ plotGraph.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from tkinter import ttk
+from Graphs import Graph
+import random
+
+g = Graph()
+g.addEdge(1,2,1)
+g.addEdge(1,3,1)
+g.addEdge(2,3,1)
+
+
+class PlotGraph:
+    
+    def plot(self,graph):
+        self.root = Tk()
+        self.root.columnconfigure(0, weight=1, minsize=75)
+        self.root.rowconfigure(0, weight=1, minsize=50)
+        self.canvas = Canvas(self.root)
+        self.canvas.grid(column=0, row=0, sticky=(N, W, E, S) )
+        self.nodeRadius = 24
+
+
+        self.samplesX = random.sample(range(1,400, 2*self.nodeRadius), len(graph.vertList))
+        self.samplesY = random.sample(range(1,400, 2*self.nodeRadius), len(graph.vertList))
+        self.vertices = zip(self.samplesX,self.samplesY,graph.vertList)
+        self.nodeCord = {}
+
+        for x,y,vert in self.vertices:
+            self.nodeCord[vert] = (x+self.nodeRadius, y+self.nodeRadius)
+            self.canvas.create_oval(x,y,x+2*self.nodeRadius, y+2*self.nodeRadius , fill='red')
+            self.canvas.create_text(x+self.nodeRadius, y+self.nodeRadius , text=vert)
+
+        for a,b,w in graph.edgeArray:
+            self.canvas.create_line(self.nodeCord[a.getId()] , self.nodeCord[b.getId()])
+        
+
+
+        self.root.mainloop()
+
+pg = PlotGraph()
+pg.plot(g)
+
+from Kruskal import Kruskal
+ng = Kruskal(g)
+gg = ng.minSpanning()
+
+kg = PlotGraph()
+kg.plot(gg)
